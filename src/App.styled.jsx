e6bac06e07f0779660b96d19d5b88db2dd0cac59
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-09-11 19:47:39 +0300

    change App.styled.jsx

---
+++ src/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin: auto;
+  max-width: 500px;
+  padding: 20px;
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, .6);
+  text-align: center;
+`;
